f9f781f6f1515970c524cf395cbe10d8d80b5ea8
SMuyesser <smuyesser@gmail.com>
2018-01-31 00:18:57 -0800

    background complete, navbar started, middle place holder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import {Grid, Jumbotron} from 'react-bootstrap';
+import {Grid, Jumbotron, Nav, Navbar, NavItem} from 'react-bootstrap';
+const logo = require('./images/FullSizeRender.jpg');
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Jumbotron>
-          <Grid>
-            <h1>My New React App</h1>
-            <p>
-              Just the beginning of the journey
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>
+    <div> 
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <img src={logo} height={50}></img>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Shop
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Blog
+          </NavItem>
+        </Nav>
+      </Navbar>;
+
+      <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+
+        <main role="main" className="inner cover">
+          <h1 className="cover-heading">Dee's Wax</h1>
+          <p className="lead">Sugar wax to get hair off backs.</p>
+          <p className="lead">
+            <a href="#" className="btn btn-lg btn-secondary">Lights. Camera. WAXtion.</a>
+          </p>
+        </main>
+
+       
+      </div> 
+    </div>    
     );
   }
 }
