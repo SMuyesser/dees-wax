d3f837accbf51c70323c24be6094dfa07d08596b
SMuyesser <smuyesser@gmail.com>
2018-02-01 18:21:10 -0800

    marketing component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Carousel from './carousel.jsx';
+import Marketing from './marketing.jsx';
 import './main.css';
-const potDrip = require('../images/IMG957811.jpg');
-const sugar = require('../images/IMG953797.jpg');
-const stubble = require('../images/IMG953193.jpg');
 const armpit = require('../images/IMG950282.jpg');
 const waxing = require('../images/IMG958330.jpg');
 
@@ -14,28 +12,7 @@ const Main = () =>
         <Carousel />
 
         <div className="container marketing">
-
-          <div className="row">
-            <div className="col-lg-4">
-              <img className="rounded marketing-pic" src={potDrip} alt="Generic placeholder" width="140" height="140" />
-              <h2>Wax</h2>
-              <p>Wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax</p>
-              <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
-            </div>{/*<!-- /.col-lg-4 -->*/}
-            <div className="col-lg-4">
-              <img className="rounded marketing-pic" src={sugar} alt="Generic placeholder" width="140" height="140" />
-              <h2>Sugar</h2>
-              <p>Sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar </p>
-              <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
-            </div>{/*<!-- /.col-lg-4 -->*/}
-            <div className="col-lg-4">
-              <img className="rounded marketing-pic" src={stubble} alt="Generic placeholder" width="140" height="140" />
-              <h2>Hair</h2>
-              <p>Hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair</p>
-              <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
-            </div>{/*<!-- /.col-lg-4 -->*/}
-          </div>{/*<!-- /.row -->*/}
-
+          <Marketing />
           <hr className="featurette-divider"/>
 
           <div className="row featurette">
