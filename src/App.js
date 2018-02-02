fd6def87b56238bd066fbd64a0723011f83046be
SMuyesser <smuyesser@gmail.com>
2018-02-01 17:31:04 -0800

    carousel component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/header.jsx';
+import Carousel from './components/carousel.jsx';
 
-const centerDrip = require('./images/cropped-center-drip.jpg');
-const leftDrip = require('./images/cropped-left-drip.jpg');
-const collage = require('./images/collage.jpg');
+
 const potDrip = require('./images/IMG957811.jpg');
 const sugar = require('./images/IMG953797.jpg');
 const stubble = require('./images/IMG953193.jpg');
@@ -20,64 +19,25 @@ class App extends Component {
 
       <main role="main">
 
-        <div id="myCarousel" className="carousel slide" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="myCarousel" data-slide-to="0" className="active"></li>
-            <li data-target="myCarousel" data-slide-to="1"></li>
-            <li data-target="myCarousel" data-slide-to="2"></li>
-          </ol>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img className="first-slide mx-auto d-block" src={collage} alt="First slide" />           
-            </div>
-            <div className="carousel-item">
-              <img className="second-slide mx-auto d-block" src={centerDrip} alt="First slide" />
-              <div className="container">
-                <div className="carousel-caption">
-                  <h1>Dee's Wax</h1>
-                  <p>The sweet way to make that fuzz dissapp-HAIR.</p>
-                  <p><a className="btn btn-lg btn-outline-dark" href="0" role="button">Learn more</a></p>
-                </div>
-              </div>            
-            </div>
-            <div className="carousel-item">
-              <img className="third-slide mx-auto d-block" src={leftDrip} alt="Second slide" />
-              <div className="container">
-                <div className="carousel-caption">
-                  <h1>Sugar Wax</h1>
-                  <p>Leaving you silky smooth after that hair is removed.</p>
-                  <p><a className="btn btn-lg btn-outline-dark" href="1" role="button">Learn more</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="myCarousel" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="myCarousel" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+        <Carousel />
 
         <div className="container marketing">
 
           <div className="row">
             <div className="col-lg-4">
-              <img className="rounded-circle" src={potDrip} alt="Generic placeholder" width="140" height="140" />
+              <img className="rounded" src={potDrip} alt="Generic placeholder" width="140" height="140" />
               <h2>Wax</h2>
               <p>Wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax wax</p>
               <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
             </div>{/*<!-- /.col-lg-4 -->*/}
             <div className="col-lg-4">
-              <img className="rounded-circle" src={sugar} alt="Generic placeholder" width="140" height="140" />
+              <img className="rounded" src={sugar} alt="Generic placeholder" width="140" height="140" />
               <h2>Sugar</h2>
               <p>Sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar sugar </p>
               <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
             </div>{/*<!-- /.col-lg-4 -->*/}
             <div className="col-lg-4">
-              <img className="rounded-circle" src={stubble} alt="Generic placeholder" width="140" height="140" />
+              <img className="rounded" src={stubble} alt="Generic placeholder" width="140" height="140" />
               <h2>Hair</h2>
               <p>Hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair hair</p>
               <p><a className="btn btn-secondary" href="" role="button">View details &raquo;</a></p>
---
+++ src/components/carousel.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './header.css';
+
+const centerDrip = require('../images/cropped-center-drip.jpg');
+const leftDrip = require('../images/cropped-left-drip.jpg');
+const collage = require('../images/collage.jpg');
+
+const Carousel = () => 
+    <div id="myCarousel" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="first-slide mx-auto d-block" src={collage} alt="First slide" />           
+        </div>
+        <div className="carousel-item">
+          <img className="second-slide mx-auto d-block" src={centerDrip} alt="First slide" />
+          <div className="container">
+            <div className="carousel-caption">
+              <h1>Dee's Wax</h1>
+              <p>The sweet way to make that fuzz dissapp-HAIR.</p>
+              <p><a className="btn btn-lg btn-outline-dark" href="javascript" role="button">Learn more</a></p>
+            </div>
+          </div>            
+        </div>
+        <div className="carousel-item">
+          <img className="third-slide mx-auto d-block" src={leftDrip} alt="Second slide" />
+          <div className="container">
+            <div className="carousel-caption">
+              <h1>Sugar Wax</h1>
+              <p>Leaving you silky smooth after that hair is removed.</p>
+              <p><a className="btn btn-lg btn-outline-dark" href="javascript" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+
+    export default Carousel;
